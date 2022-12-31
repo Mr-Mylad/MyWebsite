f125e3246a2de0e3f4b89e4a60ed2c881a471b88
Mr <mrmylad10@gmail.com>
2022-12-31 15:07:48 +0530

    Added a menubar with proper css (Probably going to edit it to make it better) Did some progress in the contact section of the website

--- Homepage/script.js
+++ Homepage/script.js
@@ -1,1 +1,14 @@
 console.log('Welcome to the homepage!');
+
+for (Item in document.getElementsByClassName('Me')) {
+    document.getElementsByClassName('Me')[Item].onclick = ItIsMe
+}
+
+function ItIsMe() {
+    Random = Math.round(Math.random(0, 1));
+    if (Random == 0) {
+        alert('Ouch! Don\'t poke me!');
+    } else {
+        alert('Hi! It\'s me!');
+    }
+}
