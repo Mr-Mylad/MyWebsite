9603a88a776b6d74626cd96a0b223419c9fd5a29
Mr. Mylad <mrmylad10@gmail.com>
2023-04-09 14:45:21 +0530

    Changes in extended discription

--- Homepage/script.js
+++ Homepage/script.js
@@ -24,3 +24,40 @@ function Youtube() {
 function Itch() {
     open('https://mylad.itch.io')
 }
+
+// Smooth scroll code
+function smoothScroll(target, duration) {
+    const targetElement = document.querySelector(target);
+    const targetPosition = targetElement.getBoundingClientRect().top;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    let startTime = null;
+  
+    function scroll(currentTime) {
+      if (startTime === null) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+      const scrollAmount = easeInOutQuad(timeElapsed, startPosition, distance, duration);
+      window.scrollTo(0, scrollAmount);
+      if (timeElapsed < duration) requestAnimationFrame(scroll);
+    }
+  
+    function easeInOutQuad(t, b, c, d) {
+      t /= d/2;
+      if (t < 1) return c/2*t*t + b;
+      t--;
+      return -c/2 * (t*(t-2) - 1) + b;
+    }
+  
+    requestAnimationFrame(scroll);
+  }
+
+  const smoothScrollLinks = document.querySelectorAll('.MenuAnchors');
+
+smoothScrollLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const target = e.target.getAttribute('href');
+    const duration = 100;
+    smoothScroll(target, duration);
+  });
+});
